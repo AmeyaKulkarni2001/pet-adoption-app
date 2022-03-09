295997ad6982c7da5954d38a024f21746b1c6091
adwait kulkarni <adwaitkulkarni2211@gmail.com>
2022-03-09 13:26:10 +0530

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import FAQ from './FAQ'
+import Search from './Search'
+import AboutUs from './AboutUs'
+
+function NavBar() {
+  return (
+    <div>
+        NavBar
+        <FAQ />
+        <Search />
+        <AboutUs />
+    </div>
+  )
+}
+
+export default NavBar
