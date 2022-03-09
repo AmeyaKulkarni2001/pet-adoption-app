717a48a33d7baf7ff239488fc5ae0ec00dc3b331
Tarak Kulkarni <tarakmkulkarni@gmail.com>
2022-03-09 14:56:59 +0530

    Added some css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import Search from './components/Search'
-
+import React from "react";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import "./App.css";
 function App() {
   return (
     <div>
       <NavBar />
       <Search />
-      <Footer />
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,20 @@ import React from "react";
 import FAQ from "./FAQ";
 import Search from "./Search";
 import AboutUs from "./AboutUs";
-import "./NavBar.css"
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <>
-      <div id="nav-container">
+    <div id="nav-container">
+      <div className="logo-link">
         <div>Logo</div>
+      </div>
+      <div className="nav-links">
         <div>FAQ</div>
-        <div>Serch</div>
+        <div>Search</div>
         <div>Login</div>
       </div>
-    </>
+    </div>
   );
 }
 
